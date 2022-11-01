18575530727ea34e1a9eed2dc496bcf6e5dbbabb
Roberto <beto.rodriguesds@gmail.com>
2022-10-31 21:45:36 -0300

    prisma, primeiro model e cors

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,27 @@
 import Fastify from  'fastify'
+import { PrismaClient } from "@prisma/client";
+import cors from '@fastify/cors'
 
+
+const prisma = new PrismaClient({
+	log: ['query']
+})
 
 async function bootstrap(){
 	const fastFy = Fastify({
 		logger: true,
 	})
 
-	fastFy.get('/pools/count', () => {
-		return { count: 0 }
+	await fastFy.register(cors, {
+		origin: true
 	})
 
-	await fastFy.listen({port: 3333})
+	fastFy.get('/pools/count', async () => {
+		const count = await prisma.pool.count()
+		return { count }
+	})
+
+	await fastFy.listen({ port: 3333, host: '0.0.0.0' })
 }
 
 bootstrap()
