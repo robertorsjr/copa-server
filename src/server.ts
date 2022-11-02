c6839c71f11584eea14421d7c9080c07794642b5
Roberto <beto.rodriguesds@gmail.com>
2022-11-02 14:16:31 -0300

    rotas de criaçao de pool, contagem de users e guesses

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import Fastify from  'fastify'
 import { PrismaClient } from "@prisma/client";
 import cors from '@fastify/cors'
+import {z} from "zod";
+import ShortUniqueId from "short-unique-id";
 
 
 const prisma = new PrismaClient({
@@ -20,8 +22,39 @@ async function bootstrap(){
 		const count = await prisma.pool.count()
 		return { count }
 	})
+
+	fastFy.get('/users/count', async () => {
+		const count = await prisma.user.count()
+		return { count }
+	})
+
+	fastFy.get('/guesses/count', async () => {
+		const count = await prisma.guess.count()
+		return { count }
+	})
+
+	fastFy.post('/pools', async (request, reply) => {
+		const createPoolBody = z.object({
+			title: z.string(),
+		})
+
+		const { title } = createPoolBody.parse(request.body)
+
+		const generate = new ShortUniqueId({ length: 6})
+		const code = String(generate()).toUpperCase()
+
+		await prisma.pool.create({
+			data: {
+				title,
+				code: code
+			}
+		})
+
+		return reply.status(201).send({ code })
+	})
+
 	await fastFy.listen({ port: 3333 })
-	//  host: '0.0.0.0' 
+	//  host: '0.0.0.0'
 }
 
 bootstrap()
