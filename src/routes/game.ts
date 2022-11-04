fecfb672bfbd147e34401e6353def6b4ea8fbdd9
Roberto <beto.rodriguesds@gmail.com>
2022-11-04 16:23:20 -0300

    rotas novas

--- src/routes/game.ts
+++ src/routes/game.ts
@@ -1,9 +1,38 @@
 import {prisma} from "../libs/prisma";
 import {FastifyInstance} from "fastify";
+import {z} from "zod";
+import {authenticate} from "../plugins/authenticate";
 
 export async function gameRoutes(fastify: FastifyInstance){
-	fastify.get('/games/count', async () => {
-		const count = await prisma.pool.count()
-		return { count }
+	fastify.get('/pools/:id/games', { onRequest: [authenticate] }, async (request) => {
+		const gamePoolRequest = z.object({
+			id: z.string()
+		})
+
+		const { id } = gamePoolRequest.parse(request.params)
+
+		const games = await prisma.game.findMany({
+			orderBy: {
+				date: 'desc',
+			},
+			include: {
+				guesses: {
+					where: {
+						participant: {
+							userId: request.user.sub,
+							poolId: id,
+						}
+					}
+				}
+			}
+		})
+
+		return { games: games.map(game => {
+			return{
+				...game,
+				guess: game.guesses.length > 0 ? game.guesses[0]  : null,
+				guesses: undefined
+			}
+			}) }
 	})
 }
--- src/routes/pool.ts
+++ src/routes/pool.ts
@@ -2,6 +2,7 @@ import {prisma} from "../libs/prisma";
 import {FastifyInstance} from "fastify";
 import {z} from "zod";
 import ShortUniqueId from "short-unique-id";
+import {authenticate} from "../plugins/authenticate";
 
 export async function poolRoutes(fastify: FastifyInstance){
 	fastify.get('/pools/count', async () => {
@@ -19,15 +20,160 @@ export async function poolRoutes(fastify: FastifyInstance){
 		const generate = new ShortUniqueId({ length: 6})
 		const code = String(generate()).toUpperCase()
 
-		await prisma.pool.create({
-			data: {
-				title,
-				code: code
-			}
-		})
+		try {
+			await request.jwtVerify()  // is authenticated
+			await prisma.pool.create({
+				data: {
+					title,
+					code: code,
+					ownerId: request.user?.sub,
+
+					participants: {
+						create: {
+							userId: request.user.sub
+						}
+					}
+				}
+			})
+		} catch { // is not authenticated
+			await prisma.pool.create({
+				data: {
+					title,
+					code: code
+				}
+			})
+		}
 
 		return reply.status(201).send({ code })
 	})
 
+	fastify.post('/pools/join',{ onRequest: [authenticate] } , async (request, reply) => {
+		const joinPoolBody = z.object({
+			code: z.string(),
+		})
+
+		const { code } = joinPoolBody.parse(request.body)
+
+		const pool = await prisma.pool.findUnique({
+			where: {
+				code,
+			},
+			include: {
+				participants: {
+					where: {
+						userId: request.user.sub,
+					}
+				}
+			}
+		})
+
+		if(!pool){
+			return reply.status(400).send({ message: 'pool not found!' })
+		}
+
+		if(pool.participants.length > 0) {
+			return reply.status(400).send({ message: 'you already joined this pool' })
+		}
+
+		if(!pool.ownerId){
+			await prisma.pool.update({
+				where: {
+					id: pool.id,
+				},
+				data: {
+					ownerId: request.user.sub,
+				}
+			})
+		}
+
+
+		await prisma.participant.create({
+			data:{
+				poolId: pool.id,
+				userId: request.user.sub,
+			}
+		})
+
+		return reply.status(201).send()
+
+	})
+
+	fastify.get('/pools', {onRequest: [authenticate] }, async(request)=> {
+		const pools = await prisma.pool.findMany({
+			where: {
+				participants: {
+					some: {
+						userId: request.user.sub
+					}
+				}
+			},
+			include: {
+				_count: {
+					select: {
+						participants: true
+					}
+				},
+				participants: {
+					select: {
+						id: true,
+						user: {
+							select: {
+								avatarUrl: true,
+							}
+						}
+					},
+					take: 4
+				},
+				owner: {  // if you want return complete owner, just set true in owner
+					select: {
+						id: true,
+						name: true
+					}
+				}
+			}
+		})
+
+		return { pools }
+	})
+
+	fastify.get('/pools/:id', { onRequest: [authenticate]}, async (request) => {
+		const getPoolParams = z.object({
+			id: z.string(),
+		})
+
+		const { id } = getPoolParams.parse(request.params)
+
+		const pool = await prisma.pool.findMany({
+			where: {
+				id,
+			},
+			include: {
+				_count: {
+					select: {
+						participants: true
+					}
+				},
+				participants: {
+					select: {
+						id: true,
+						user: {
+							select: {
+								avatarUrl: true,
+							}
+						}
+					},
+					take: 4
+				},
+				owner: {  // if you want return complete owner, just set true in owner
+					select: {
+						id: true,
+						name: true
+					}
+				}
+			}
+		})
+
+		return { pool }
+	})
 }
 
